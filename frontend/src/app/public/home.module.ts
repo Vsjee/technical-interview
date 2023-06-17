a0c308cd9a82f8e5710b4c8cb61a7f5882a551d4
david hernandez <davidfelipehernandez1@gmail.com>
2023-06-17 16:59:55 -0500

    min

--- frontend/src/app/public/home.module.ts
+++ frontend/src/app/public/home.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 import { HomeComponent } from './pages/home/home.component';
 import { SigninComponent } from './pages/signin/signin.component';
 import { SignupComponent } from './pages/signup/signup.component';
@@ -7,6 +10,12 @@ import { HomeRoutingModule } from './home-routing.module';
 
 @NgModule({
   declarations: [HomeComponent, SigninComponent, SignupComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+  ],
 })
 export class HomeModule {}
