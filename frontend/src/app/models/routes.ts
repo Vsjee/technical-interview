72dbdd50d90a62e10eb9be4ed276f61423c8f8fe
david hernandez <davidfelipehernandez1@gmail.com>
2023-06-17 13:42:46 -0500

    add navbar component

---
+++ frontend/src/app/models/routes.ts
@@ -0,0 +1,10 @@
+export const publicRoutes = {
+  HOME: '/public/home',
+  SIGNIN: '/public/signin',
+  SINGUP: '/public/signup',
+};
+
+export const privateRoutes = {
+  DASHBOARD: '/private/dashboard',
+  PROFILE: '/private/profile',
+};
---
+++ frontend/src/app/shared/nabvar/nabvar.component.ts
@@ -0,0 +1,49 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { NavigationEnd, Router } from '@angular/router';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { privateRoutes, publicRoutes } from 'src/app/models';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule, AppRoutingModule, MatIconModule],
+  selector: 'app-nabvar',
+  templateUrl: './nabvar.component.html',
+  styleUrls: ['./nabvar.component.scss'],
+})
+export class NabvarComponent {
+  publicRoutes = publicRoutes;
+  privateRoutes = privateRoutes;
+  hiddeNavbar: boolean = false;
+  changeNavContent: boolean = false;
+
+  constructor(private router: Router) {
+    router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        if (
+          event.url === this.publicRoutes.SIGNIN ||
+          event.url === this.publicRoutes.SINGUP
+        ) {
+          this.hiddeNavbar = true;
+        } else {
+          this.hiddeNavbar = false;
+        }
+
+        if (
+          event.url === privateRoutes.DASHBOARD ||
+          event.url === privateRoutes.PROFILE
+        ) {
+          this.hiddeNavbar = true;
+          this.changeNavContent = true;
+        } else {
+          this.changeNavContent = false;
+        }
+      }
+    });
+  }
+
+  navigate(route: string) {
+    this.router.navigate([route]);
+  }
+}
