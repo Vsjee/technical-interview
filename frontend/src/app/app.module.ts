32da3ac2f773cdef067224f204084466c2c89d8e
david hernandez <davidfelipehernandez1@gmail.com>
2023-06-17 13:41:55 -0500

    add angular material

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NabvarComponent } from './shared/nabvar/nabvar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, NabvarComponent, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
