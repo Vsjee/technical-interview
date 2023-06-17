dad41167d4a69aae510db71c008331b23fa40b5b
david hernandez <davidfelipehernandez1@gmail.com>
2023-06-17 16:57:13 -0500

    add login and register desing

--- frontend/src/app/public/pages/signin/signin.component.ts
+++ frontend/src/app/public/pages/signin/signin.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { publicRoutes } from 'src/app/models';
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.scss']
+  styleUrls: ['./signin.component.scss'],
 })
 export class SigninComponent {
+  publicRoutes = publicRoutes;
+  hide: boolean = true;
 
+  constructor(private router: Router) {}
+
+  navigate(route: string) {
+    this.router.navigate([route]);
+  }
 }
--- frontend/src/app/public/pages/signup/signup.component.ts
+++ frontend/src/app/public/pages/signup/signup.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { publicRoutes } from 'src/app/models';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent {
+  publicRoutes = publicRoutes;
+  hide: boolean = true;
 
+  constructor(private router: Router) {}
+
+  navigate(route: string) {
+    this.router.navigate([route]);
+  }
 }
