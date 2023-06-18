f0e702983093a2c56a7e7c6783daca4ea9d0c6df
david hernandez <davidfelipehernandez1@gmail.com>
2023-06-17 19:45:34 -0500

    add home desing

--- frontend/src/app/public/pages/home/home.component.ts
+++ frontend/src/app/public/pages/home/home.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import {
+  fadeInRightOnEnterAnimation,
+  fadeInLeftOnEnterAnimation,
+} from 'angular-animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [fadeInRightOnEnterAnimation(), fadeInLeftOnEnterAnimation()],
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
