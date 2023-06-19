c1ee3b8ca2bf2e62de97845cfc785c4f9cbf2c2f
david hernandez <davidfelipehernandez1@gmail.com>
2023-06-18 21:26:45 -0500

    update interface

--- frontend/src/app/interfaces/teams.interface.ts
+++ frontend/src/app/interfaces/teams.interface.ts
@@ -1,20 +1,24 @@
 export interface ITeams {
-  _id: string;
-  team: {
-    id: number;
-    name: string;
-    country: string;
-    founded: number;
-    national: boolean;
-    logo: string;
-  };
-  venue: {
-    id: number;
-    name: string;
-    address: string;
-    city: string;
-    capacity: number;
-    surface: string;
-    image: string;
-  };
+  _id?: string;
+  team: ITeam;
+  venue: IVenue;
+}
+
+export interface ITeam {
+  id: number;
+  name: string;
+  country: string;
+  founded: number;
+  national: boolean;
+  logo: string;
+}
+
+export interface IVenue {
+  id: number;
+  name: string;
+  address: string;
+  city: string;
+  capacity: number;
+  surface: string;
+  image: string;
 }
